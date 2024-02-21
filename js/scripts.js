33f3fb4bc07e80ca274fe801f667d99571cffd74
Nuyaaaaaaa <Danilaok13@mail.ru>
2024-02-21 11:01:53 +0300

    1
    1

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,17 @@ $(window).on("scroll", function () {
     if( scrolled <= 200 ) {
       $(".header-wrap").removeClass("active");
     }
-   
-  });
+   // мобильное меню
+$('.menu-humb').on('click', function () {
+  $(this).toggleClass('active');
+  $('.menu-mob').toggleClass('active');
+  $('.header-wrap').toggleClass('menu_active');
+});
+//close mob menu
+$("#btn-close").click(function(){
+  $('.menu-mob').removeClass('active');
+  $('.header-wrap').removeClass('menu_active');
+  $("#menu-mob").hide();
+
+})
+});
