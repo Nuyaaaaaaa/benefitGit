1a446cc592695c17ff0640fd3c42489c9a78031e
Nuyaaaaaaa <Danilaok13@mail.ru>
2024-02-21 11:30:28 +0300

    1
    1

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
+jQuery(document).ready(function () {
 // Header fixed animated
 $(window).on("scroll", function () {
-    let scrolled = $(this).scrollTop();
-    if( scrolled > 200 ) {
-      $(".header-wrap").addClass("active");
-    }
-    if( scrolled <= 200 ) {
-      $(".header-wrap").removeClass("active");
-    }
-   // мобильное меню
-$('.menu-humb__wrap').on('click', function () {
-  $('.menu-humb').addClass('active');
-  $('.menu-mob').addClass('active');
-  $('.header-wrap').addClass('menu_active');
-});
-//close mob menu
-$("#btn-close").on('click', function() {
-  $('.menu-mob').removeClass('active');
-  $('.header-wrap').removeClass('menu_active');
-})
+  let scrolled = $(this).scrollTop();
+  if (scrolled > 200) {
+    $(".header-wrap").addClass("active");
+  }
+  if (scrolled <= 200) {
+    $(".header-wrap").removeClass("active");
+  }
+  });
+
+
+  
+  // мобильное меню
+  $('.menu-humb__wrap').on('click', function () {
+    $('.menu-humb').addClass('active');
+    $('.menu-mob').addClass('active');
+    $('.header-wrap').addClass('menu_active');
+  });
+
+
+
+
+  //close mob menu
+  $("#btn-close").on('click', function () {
+    $('.menu-mob').removeClass('active');
+    $('.header-wrap').removeClass('menu_active');
+  })
 });
